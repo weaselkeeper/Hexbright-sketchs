92d3e7e68d18fa9aa51e7f815ac027efbc399816
Jim Richardson <weaselkeeper@gmail.com>
2014-12-21 00:20:19 -0800

    select mode by tilting and pressing button

---
+++ src/hexbright_tilt/hexbright_tilt.ino
@@ -0,0 +1,262 @@
+/*
+
+  Modified Factory firmware for HexBright FLEX
+  v2.4  Dec 6, 2012
+
+
+
+When light off, long press will turn on mode until released. 
+Short press will turn on mode.
+When on, short press changes mode.
+long press turns off.
+
+light up, medium
+light down, low
+light horizontal, high
+
+*/
+
+#include <math.h>
+#include <Wire.h>
+
+// Settings
+#define OVERTEMP                340
+// Pin assignments
+#define DPIN_RLED_SW            2
+#define DPIN_GLED               5
+#define DPIN_PWR                8
+#define DPIN_DRV_MODE           9
+#define DPIN_DRV_EN             10
+#define APIN_TEMP               0
+#define APIN_CHARGE             3
+// Modes
+#define MODE_OFF                0
+#define MODE_VLOW               1
+#define MODE_LOW                2
+#define MODE_MED                3
+#define MODE_HIGH               4
+#define MODE_BLINKING           5
+#define MODE_BLINKING_PREVIEW   6
+#define MODE_ANGLEDOWN          7
+// Accel stuff
+#define ACC_ADDRESS             0x4C
+#define ACC_REG_XOUT            0
+#define ACC_REG_YOUT            1
+#define ACC_REG_ZOUT            2
+#define ACC_REG_TILT            3
+#define ACC_REG_INTS            6
+#define ACC_REG_MODE            7
+
+
+// State
+byte mode = 0;
+unsigned long btnTime = 0;
+boolean btnDown = false;
+
+
+void setup()
+{
+  // We just powered on!  That means either we got plugged
+  // into USB, or the user is pressing the power button.
+  pinMode(DPIN_PWR,      INPUT);
+  digitalWrite(DPIN_PWR, LOW);
+
+  // Initialize GPIO
+  pinMode(DPIN_RLED_SW,  INPUT);
+  pinMode(DPIN_GLED,     OUTPUT);
+  pinMode(DPIN_DRV_MODE, OUTPUT);
+  pinMode(DPIN_DRV_EN,   OUTPUT);
+  digitalWrite(DPIN_DRV_MODE, LOW);
+  digitalWrite(DPIN_DRV_EN,   LOW);
+
+  // Initialize serial busses
+  Serial.begin(9600);
+  Wire.begin();
+
+  // Configure accelerometer
+  byte config[] = {
+    ACC_REG_INTS,  // First register (see next line)
+    0xE4,  // Interrupts: shakes, taps
+    0x00,  // Mode: not enabled yet
+    0x00,  // Sample rate: 120 Hz
+    0x0F,  // Tap threshold
+    0x10   // Tap debounce samples
+  };
+  Wire.beginTransmission(ACC_ADDRESS);
+  Wire.write(config, sizeof(config));
+  Wire.endTransmission();
+
+  // Enable accelerometer
+  byte enable[] = {ACC_REG_MODE, 0x01};  // Mode: active!
+  Wire.beginTransmission(ACC_ADDRESS);
+  Wire.write(enable, sizeof(enable));
+  Wire.endTransmission();
+
+
+  btnTime = millis();
+  btnDown = digitalRead(DPIN_RLED_SW);
+  mode = MODE_OFF;
+
+  Serial.println("Powered up!");
+}
+
+void loop()
+{
+  static unsigned long lastTempTime;
+  static unsigned long lastanglecheck;
+  unsigned long time = millis();
+
+  if (time-lastanglecheck > 1000)// recheck angle
+  {
+  lastanglecheck = time;
+  int angle = readAccelAngleXZ();
+  Serial.println(angle);
+  }
+
+  // Check the state of the charge controller
+  int chargeState = analogRead(APIN_CHARGE);
+  if (chargeState < 128)  // Low - charging
+  {
+    digitalWrite(DPIN_GLED, (time&0x0100)?LOW:HIGH);
+  }
+  else if (chargeState > 768) // High - charged
+  {
+    digitalWrite(DPIN_GLED, HIGH);
+  }
+  else // Hi-Z - shutdown
+  {
+    digitalWrite(DPIN_GLED, LOW);
+  }
+
+  // Check the temperature sensor
+  if (time-lastTempTime > 1000)
+  {
+    lastTempTime = time;
+    int temperature = analogRead(APIN_TEMP);
+    Serial.print("Temp: ");
+    Serial.println(temperature);
+    if (temperature > OVERTEMP && mode != MODE_OFF)
+    {
+      Serial.println("Overheating!");
+
+      for (int i = 0; i < 6; i++)
+      {
+        digitalWrite(DPIN_DRV_MODE, LOW);
+        delay(100);
+        digitalWrite(DPIN_DRV_MODE, HIGH);
+        delay(100);
+      }
+      digitalWrite(DPIN_DRV_MODE, LOW);
+
+      mode = MODE_LOW;
+    }
+  }
+
+  // Periodically pull down the button's pin, since
+  // in certain hardware revisions it can float.
+  pinMode(DPIN_RLED_SW, OUTPUT);
+  pinMode(DPIN_RLED_SW, INPUT);
+  byte newBtnDown = digitalRead(DPIN_RLED_SW);
+
+  if (btnDown && (readAccelAngleXZ() < -9))
+    mode = MODE_VLOW;
+  else if (btnDown && (readAccelAngleXZ() < 0))
+    mode = MODE_HIGH;
+  else if (btnDown && (readAccelAngleXZ() > 5))
+    mode = MODE_MED;
+
+
+  if (btnDown && !newBtnDown && (time-btnTime)>500)
+    mode = MODE_OFF;
+  
+   // Do the mode transitions
+    switch (mode)
+    {
+    case MODE_OFF:
+      Serial.println("Mode = off");
+      pinMode(DPIN_PWR, OUTPUT);
+      digitalWrite(DPIN_PWR, LOW);
+      digitalWrite(DPIN_DRV_MODE, LOW);
+      digitalWrite(DPIN_DRV_EN, LOW);
+      break;
+    case MODE_LOW:
+      Serial.println("Mode = low");
+      pinMode(DPIN_PWR, OUTPUT);
+      digitalWrite(DPIN_PWR, HIGH);
+      digitalWrite(DPIN_DRV_MODE, LOW);
+      analogWrite(DPIN_DRV_EN, 64);
+      break;
+    case MODE_VLOW:
+      Serial.println("Mode = vlow");
+      pinMode(DPIN_PWR, OUTPUT);
+      digitalWrite(DPIN_PWR, HIGH);
+      digitalWrite(DPIN_DRV_MODE, LOW);
+      analogWrite(DPIN_DRV_EN, 4);
+      break;
+    case MODE_MED:
+      Serial.println("Mode = medium");
+      pinMode(DPIN_PWR, OUTPUT);
+      digitalWrite(DPIN_PWR, HIGH);
+      digitalWrite(DPIN_DRV_MODE, LOW);
+      analogWrite(DPIN_DRV_EN, 255);
+      break;
+    case MODE_HIGH:
+      Serial.println("Mode = high");
+      pinMode(DPIN_PWR, OUTPUT);
+      digitalWrite(DPIN_PWR, HIGH);
+      digitalWrite(DPIN_DRV_MODE, HIGH);
+      analogWrite(DPIN_DRV_EN, 255);
+      break;
+    case MODE_BLINKING:
+    case MODE_BLINKING_PREVIEW:
+      Serial.println("Mode = blinking");
+      pinMode(DPIN_PWR, OUTPUT);
+      digitalWrite(DPIN_PWR, HIGH);
+      digitalWrite(DPIN_DRV_MODE, HIGH);
+      break;
+    }
+
+  // Remember button state so we can detect transitions
+  if (newBtnDown != btnDown)
+  {
+    btnTime = time;
+    btnDown = newBtnDown;
+    delay(50);
+  }
+}
+
+
+void readAccel(char *acc)
+{
+  while (1)
+  {
+    Wire.beginTransmission(ACC_ADDRESS);
+    Wire.write(ACC_REG_XOUT);
+    Wire.endTransmission(false);       // End, but do not stop!
+    Wire.requestFrom(ACC_ADDRESS, 3);  // This one stops.
+
+    for (int i = 0; i < 3; i++)
+    {
+      if (!Wire.available())
+        continue;
+      acc[i] = Wire.read();
+      if (acc[i] & 0x40)  // Indicates failed read; redo!
+        continue;
+      if (acc[i] & 0x20)  // Sign-extend
+        acc[i] |= 0xC0;
+    }
+    break;
+  }
+}
+
+int readAccelAngleXZ()
+{
+  char acc[3];
+  int angle;
+  readAccel(acc);
+  //angle of lens up/down.
+  angle = acc[1];
+  //return atan2(acc[0], acc[2]);
+  return angle;
+}
+
